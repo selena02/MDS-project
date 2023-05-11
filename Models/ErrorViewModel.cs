8b9ac1848f9733fc6685813c30b37c574b970ca1
Antonia Oancea <92017076+AntoniaOancea@users.noreply.github.com>
2023-05-11 11:00:21 +0300

    Delete Models directory

--- Models/ErrorViewModel.cs
+++
@@ -1,9 +0,0 @@
-namespace MDS.Models
-{
-    public class ErrorViewModel
-    {
-        public string? RequestId { get; set; }
-
-        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
-    }
-}
